fdd704dd21d6385f76f0458af354359b7b9054ab
josip8 <josip8grga@gmail.com>
2019-02-19 23:10:17 +0100

    Add html pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'pleaseSignIn.html'));
 });
+
+app.post('/team_name_url', (req, res) => {
+  console.log(req.body.name_field);
+  res.end();
+})
 
 app.listen(process.env.PORT, () =>
   console.log(`EwilTwin app listening on port ${process.env.PORT}!`),
